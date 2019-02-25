2dd285ee14fc8313818586b6b40a40fc30fcfd4e
Taylor Kettle <tin.teapot@gmail.com>
2019-02-25 18:42:27 +1300

    added precipitation function

--- src/index.js
+++ src/index.js
@@ -1,23 +1,63 @@
-module.exports = parseCode
-
-function parseCode ({ code, format }) {
-  if (!code || !format) throw new Error('Must pass both code and format')
-
-  let result = {}
-
-  let [
-    section0,
-    section1,
-    section2,
-    section3,
-    section4,
-    section5,
-    section6,
-    section7
-  ] = code.split(' ')
-
-  if (section0) result.stationNumber = section0
-
-  if (section1 && format === 'FM_12') {
+function getPrecipitation ({ code = '' }) {
+  if (!code || code === '') {
+    throw new Error('Expected non empty string to be passed')
   }
+  let rainSection = code.match(/[ ]6[\d]{4}[ ]/g)
+  if (!rainSection) throw new Error('Could not find any rain sections')
+
+  let results = rainSection.map(rain => {
+    rain = rain.trim()
+    let RRR = Number(rain.slice(1, 4))
+    let tR = Number(rain[4])
+
+    // Getting precipitation data
+    let amountOfPrecipitation
+    if (RRR < 990) {
+      amountOfPrecipitation = RRR
+    } else if (RRR === 990) {
+      amountOfPrecipitation = 0
+    } else {
+      amountOfPrecipitation = (RRR - 990) / 10
+    }
+
+    // Getting time data
+    let hoursPrecedingObservation
+    switch (tR) {
+      case 1:
+        hoursPrecedingObservation = 6
+        break
+      case 2:
+        hoursPrecedingObservation = 12
+        break
+      case 3:
+        hoursPrecedingObservation = 18
+        break
+      case 4:
+        hoursPrecedingObservation = 24
+        break
+      case 5:
+        hoursPrecedingObservation = 1
+        break
+      case 6:
+        hoursPrecedingObservation = 2
+        break
+      case 7:
+        hoursPrecedingObservation = 3
+        break
+      case 8:
+        hoursPrecedingObservation = 9
+        break
+      case 9:
+        hoursPrecedingObservation = 15
+        break
+      default:
+        hoursPrecedingObservation = 3
+    }
+
+    return { hoursPrecedingObservation, amountOfPrecipitation }
+  })
+
+  return results
 }
+
+module.exports = { getPrecipitation }
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,13 +1,13 @@
-const parseCode = require("../src");
+const { getPrecipitation } = require('../src')
 
-describe("Testing metservice codes", () => {
-  it("Should successfully parse a surface recording", () => {
+describe('Testing metservice codes', () => {
+  it('Should successfully parse a surface recording', () => {
     console.log(
-      parseCode({
+      getPrecipitation({
         code:
-          "93023 16/// /1510 10210 20112 40128 52012 69944 333 20127 70004=",
-        format: "FM_12"
+          '93023 16/// /1510 10210 20112 40128 52012 69944 333 20127 70004=',
+        format: 'FM_12'
       })
-    );
-  });
-});
+    )
+  })
+})
